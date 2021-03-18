61da664f256b09db01597ce52f2e79ceca6345bc
Sou-786 <63981549+Sou-786@users.noreply.github.com>
2021-03-18 12:48:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from youtube_statistics import YTstats
 
 
-API_KEY = 'AIzaSyDz8GYHVFFQ5G1n4nL1ss9F-JyW2y0f--4'
+API_KEY = '##########################################'
 channel_id = 'UCGXsku75ELWndSG7kEihspg'
 
 yt = YTstats(API_KEY, channel_id)
